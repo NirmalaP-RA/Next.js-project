f311936e7b5f8112e9d3fb236ec5c6c4a0e247ab
nirmala parihar <nirmalapython725@gmail.com>
2025-08-30 02:15:17 +0530

    commit

--- src/app/(website)/components/ProductSection.jsx
+++ src/app/(website)/components/ProductSection.jsx
@@ -1,9 +1,10 @@
-"use client"
+// "use client"
 import React from 'react'
 import ProductCard from '../common components/ProductCard'
 
 
-export default function ProductSection(title,productData) {
+export default function ProductSection({title,productData}) {
+ 
   return (
     <>
        <p className="mx-auto mt-10 mb-5 max-w-[1200px] px-5">{title}</p>
@@ -13,14 +14,14 @@ export default function ProductSection(title,productData) {
             className="mx-auto grid max-w-[1200px] grid-cols-2 gap-3 px-5 pb-10 lg:grid-cols-4"
           >
             {/* <!-- 1 --> */}
-      {
-        productData.map((v,i)=>{
-          return(
-            <ProductCard key={i}/>
-          )
-        })
-      }
-           
+              {
+                                  productData.map((v,i)=>{
+                                    return(
+                                      <ProductCard key={i}/>
+                                    )
+                                  })
+                                }
+                
           
             </section>
           
